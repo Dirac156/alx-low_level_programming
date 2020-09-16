a75919515af706451f7da6ef49da8c9a7453e46a
Dirac Murairi <diracm8@gmail.com>
2020-09-16 17:42:47 +0200

    dirac

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,12 @@
  */
 int main(void)
 {
-int i;
-long int li;
-long long int Li;
-char c;
-float f;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(i));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(Li));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of a int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 return (0);
 }
