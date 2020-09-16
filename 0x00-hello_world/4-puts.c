fc8535005563b7222a565f7bdab7c46d84537769
Dirac Murairi <diracm8@gmail.com>
2020-09-16 15:47:17 +0200

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
+/*This is the entry point*/
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
