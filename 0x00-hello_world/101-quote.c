73578c862b1ad5c7d1f29a2b6dbf1c5339827c89
Dirac Murairi <diracm8@gmail.com>
2020-09-16 19:01:57 +0200

    final

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * main - Entry point
  * Return:1
  */
 int main(void)
 {
-char s[100] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-sprintf(stderr, "%s", s);
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" ;
+setenv("ShellVar", ch, 1) ;
+system("echo $ShellVar >&2") ;
 return (1);
 }
