39452a0fb4c7703b07aa1cdadf15c11c73b8e0fd
Dirac Murairi <diracm8@gmail.com>
2020-09-16 15:58:14 +0200

    dirac

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,6 @@
 #include <string.h>
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle \n");
+puts("\"Programming is like building a multilingual puzzle,\n");
 return (0);
 }
