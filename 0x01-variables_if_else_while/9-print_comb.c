5e5db6c8e5be20b0feb562869d04b8d78e84d765
Dirac Murairi <diracm8@gmail.com>
2020-09-17 18:34:56 +0200

    dirac

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return:0
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar (i + '0');
+if (i < 9)
+{
+putchar (',');
+putchar (' ');
+}
+}
+putchar('\n');
+return (0);
+}
