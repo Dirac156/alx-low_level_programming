383642bbebbb071c40828e062832b7b69343a2af
Dirac Murairi <diracm8@gmail.com>
2020-09-17 16:00:25 +0200

    dirac

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,15 +18,15 @@ int main(void)
 	/* your code goes there */
 if (n > 0)
 {
-printf("%u is positive\n", n);
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("%u is zero\n", n);
+printf("%d is zero\n", n);
 }
 else
 {
-printf("%u is negative\n", n);
+printf("%d is negative\n", n);
 }
 return (0);
 }
