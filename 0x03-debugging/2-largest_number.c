96fc9fb7498cb4b388955acbbf332b50fd84c84f
Dirac Murairi <diracm8@gmail.com>
2020-09-22 22:23:12 +0200

    d

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,13 +12,25 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if (a > b && a > c)
+if (a > b && b > c)
 {
 largest = a;
+}
+else if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > a && a > c)
+{
+largest = b;
 }
 else if (b > a && b > c)
 {
 largest = b;
+}
+else if (c > a && a > b)
+{
+largest = c;
 }
 else if (c > a && c > b)
 {
