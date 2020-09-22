8df295b2c80485c462a5d7f9dda4cde42dbf7aaf
Dirac Murairi <diracm8@gmail.com>
2020-09-22 15:34:26 +0200

    dirac

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,13 +1,5 @@
-#include <unistd.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+extern int _putchar(char c);
 
-/**
-* _putchar - writes the character c to stdout
-* @c: The character to print
-*
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
-*/
-int _putchar(char c)
-{
- return (write(1, &c, 1));
-}
+#endif
