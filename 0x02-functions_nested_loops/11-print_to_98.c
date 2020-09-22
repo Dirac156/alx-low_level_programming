56016ee7b90224b5578c0807380f9c6b541e2918
Dirac Murairi <diracm8@gmail.com>
2020-09-22 21:10:44 +0200

    d

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98- print_alphabet
+ *@n: variable
+ * Return: 0 or 1
+ */
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+while (n <= 98)
+{
+printf("%d", n);
+n++;
+if (n < 99)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+}
+}
+
+else if (n >= 98)
+{
+while (n >= 98)
+{
+printf("%d, ", n);
+if (n != 97)
+{
+printf(", ");
+}
+else
+{
+printf("\n");
+}
+n--;
+
+}
+}
+
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -11,4 +11,5 @@ extern int print_last_digit(int);
 extern void jack_bauer(void);
 extern void times_table(void);
 extern int add(int, int);
+extern void print_to_98(int n);
 #endif
