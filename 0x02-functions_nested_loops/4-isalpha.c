0bcf0e1f2c78e71484714b9ba400a7158db2f696
Dirac Murairi <diracm8@gmail.com>
2020-09-22 17:09:47 +0200

    d

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,11 @@
  */
 int _isalpha(int c)
 {
-if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else if (c >= 97 && c <= 122)
 {
 return (1);
 }
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * main - check the code for Holberton School students.
  *
@@ -8,11 +9,13 @@ int main(void)
 {
     int r;
 
-    r = _islower('H');
+    r = _isalpha('H');
     _putchar(r + '0');
-    r = _islower('o');
+    r = _isalpha('o');
     _putchar(r + '0');
-    r = _islower(108);
+    r = _isalpha(108);
+    _putchar(r + '0');
+    r = _isalpha(';');
     _putchar(r + '0');
     _putchar('\n');
     return (0);
