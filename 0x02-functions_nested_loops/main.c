8ae89156bb350332d3ee5ef060f87bf01e8df76e
Dirac Murairi <diracm8@gmail.com>
2020-09-22 19:20:05 +0200

    d

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -7,12 +7,6 @@
  */
 int main(void)
 {
-    int r;
-
-    print_last_digit(98);
-    print_last_digit(0);
-    r = print_last_digit(-1024);
-    _putchar('0' + r);
-    _putchar('\n');
+    jack_bauer();
     return (0);
 }
