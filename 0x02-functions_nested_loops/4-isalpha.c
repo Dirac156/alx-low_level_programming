149707099ff3741bc84c9944dd8e76a9a544b3dc
Dirac Murairi <diracm8@gmail.com>
2020-09-22 17:07:11 +0200

    d

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _isalpha- print_alphabet
+ *@c : the character
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -4,4 +4,5 @@ extern int _putchar(char c);
 extern void print_alphabet(void);
 extern void print_alphabet_x10(void);
 extern int _islower(int c);
+extern int _isalpha(int c);
 #endif
