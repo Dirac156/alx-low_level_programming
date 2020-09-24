f54097d2ae2a4ce46f6b5e4c9e4228b577035bf9
Dirac Murairi <diracm8@gmail.com>
2020-09-24 22:16:22 +0200

    d

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - check the code for Holberton School students.
+ *@size: variable
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+int count, i;
+count = 0;
+while (count < size)
+{
+i = size;
+while (i > count + 1)
+{
+i--;
+_putchar(' ');
+}
+while (i > 0)
+{
+_putchar('#');
+i--;
+}
+count++;
+_putchar('\n');
+}
+}
