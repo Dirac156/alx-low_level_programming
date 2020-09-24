aa3320cfcbf5a577b1e6421fc09f981472b965f9
Dirac Murairi <diracm8@gmail.com>
2020-09-24 19:00:30 +0200

    dirac

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isdigit - check the code for Holberton School students.
+ *@c : variable
+ * Return: 0 or 1 .
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,4 +1,5 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 int _isupper(int c);
+int _isdigit(int c);
 #endif
