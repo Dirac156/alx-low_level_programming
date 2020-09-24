3f5bed166f96d772200e9b4d22f14c4e7885a8eb
Dirac Murairi <diracm8@gmail.com>
2020-09-24 22:43:55 +0200

    d

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
+int i = 1;
+while (i <= 100)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
@@ -26,6 +26,7 @@ else
 {
 printf("%d ", i);
 }
+i++;
 }
 printf("\n");
 return (0);
