f6f8239ceb1d5af53add506a82b8256030db85ee
Dirac Murairi <diracm8@gmail.com>
2020-09-24 19:20:47 +0200

    di

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - check the code for Holberton School students.
+ *@a : variable
+ *@b : variable
+ * Return: 0 or 1 .
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}
