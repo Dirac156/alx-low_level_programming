469f0a47ee61fd48377eb11300ffdb0222a4e776
Dirac Murairi <diracm8@gmail.com>
2020-09-24 22:32:25 +0200

    d

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,19 +11,19 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
 {
-printf("FizzBuzz");
+printf("FizzBuzz ");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz");
+printf("Fizz ");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz");
+printf("Buzz ");
 }
 else
 {
-printf("%d", i);
+printf("%d ", i);
 }
 }
 printf("\n");
