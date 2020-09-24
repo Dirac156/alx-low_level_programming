814a06eb8b932d6ae70ebd006850f48071a4d955
Dirac Murairi <diracm8@gmail.com>
2020-09-24 18:54:03 +0200

    d

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code for Holberton School students.
+ *
+ * Return: 0 or 1 .
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,4 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+int _isupper(int c);
+#endif
