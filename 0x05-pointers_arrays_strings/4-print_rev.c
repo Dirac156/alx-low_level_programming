44c1737e45fa970b44112d73e77d9d4955e56319
Dirac Murairi <diracm8@gmail.com>
2020-09-30 13:38:16 +0200

    d:

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_rev - check the code for Holberton School students.
+ * @s: variable.
+ */
+void print_rev(char *s)
+{
+int i;
+int a;
+for (i = 0; s[i] != '\0'; i++)
+{
+continue;
+}
+for (a = 0; a <= i; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
