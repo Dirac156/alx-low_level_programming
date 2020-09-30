2f7bc448babb3582a1fb443fbc7121c300bf33cb
Dirac Murairi <diracm8@gmail.com>
2020-09-30 13:04:57 +0200

    d

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: variable.
+ *
+ *Return: the lenght of the string
+ */
+int _strlen(char *s)
+{
+int lenght;
+lenght = strlen(s);
+return (lenght);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -2,4 +2,5 @@
 #define HOLBERTON_H
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
 #endif
