bfb78091efe175cea18bbe36f4c5407283316f64
Dirac Murairi <diracm8@gmail.com>
2020-09-30 12:31:58 +0200

    d

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - Entry pont
+ */
+
+
+
+int main()
+{
+    int i, j, isPrime;
+    long int num;
+    /* Input a number from user */
+
+  num = 612852475143;
+
+    printf("All Prime Factors of %ld are: \n", num);
+
+    /* Find all Prime factors */
+    for(i=2; i<=num; i++)
+    {
+        /* Check 'i' for factor of num */
+        if(num%i==0)
+        {
+            /* Check 'i' for Prime */
+            isPrime = 1;
+            for(j=2; j<=i/2; j++)
+            {
+                if(i%j==0)
+                {
+                    isPrime = 0;
+                    break;
+                }
+            }
+
+            /* If 'i' is Prime number and factor of num */
+            if(isPrime==1)
+            {
+                printf("%d, ", i);
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ *reset_to_98 - main function
+ *@n : our pointer
+ */
+
+void reset_to_98(int *n)
+{
+*n = 89;
+}
