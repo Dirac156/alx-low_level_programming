f6a07ca4ca42910fab0dcf538f69e7906f8fbead
Dirac Murairi <diracm8@gmail.com>
2020-10-01 21:59:28 +0200

    d

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * cap_string - check the code for Holberton School students.
+ *@n : variable
+ * Return: return
+ */
+
+char *cap_string(char *n)
+{
+int i = 0;
+
+while (n[i] != '\0')
+{
+if (n[i] == 32 || n[i] == 46 || n[i] == '\t' || n[i] == '\n'
+|| n[i] == 44 || n[i] == 59 || n[i] == '!' || n[i] == '?'
+|| n[i] == '(' || n[i] == ')' || n[i] == '{' || n[i] == '}')
+{
+if (n[i + 1] <= 122 && n[i + 1] >= 97)
+{
+n[i + 1] = n[i + 1] - 32;
+}
+}
+i++;
+}
+return (n);
+}
