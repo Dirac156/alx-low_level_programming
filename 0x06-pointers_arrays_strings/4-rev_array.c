f19feedbaaa4bc3e9d672e118bba28e60439a05f
Dirac Murairi <diracm8@gmail.com>
2020-10-01 21:00:57 +0200

    d

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,7 +4,7 @@
 /**
  * reverse_array - check the code for Holberton School students.
  *@n : variable
- *@n : variable
+ *@a : variable
  */
 
 void reverse_array(int *a, int n)
@@ -14,21 +14,21 @@ int tem;
 int b = n;
 if (n % 2 == 0)
 {
-for (i = 0; i < n/2; i++ )
+for (i = 0; i < n / 2; i++)
 {
 tem = a[i];
-a[i] = a[b -1];
-a[b -1] = tem;
+a[i] = a[b - 1];
+a[b - 1] = tem;
 b--;
 }
 }
 else if (n % 2 == 1)
 {
-for (i = 0; i < (n -1)/2; i++ )
+for (i = 0; i < (n - 1) / 2; i++)
 {
 tem = a[i];
-a[i] = a[b -1];
-a[b -1] = tem;
+a[i] = a[b - 1];
+a[b - 1] = tem;
 b--;
 }
 }
