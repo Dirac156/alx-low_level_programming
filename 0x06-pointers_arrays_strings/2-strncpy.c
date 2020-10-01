9a4488821cd963e6fb07bfee5dc20e348f46c58d
Dirac Murairi <diracm8@gmail.com>
2020-10-01 22:57:28 +0200

    d

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strncat - check the code for Holberton School students.
+ * _strncpy - check the code for Holberton School students.
  *@dest : variable
  *@src : variable
  *@n : the integer
