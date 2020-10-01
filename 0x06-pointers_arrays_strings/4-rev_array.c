af29e067d5fdc4c2008fef75d571556382ae9706
Dirac Murairi <diracm8@gmail.com>
2020-10-01 20:57:47 +0200

    d

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,11 +12,24 @@ void reverse_array(int *a, int n)
 int i;
 int tem;
 int b = n;
+if (n % 2 == 0)
+{
 for (i = 0; i < n/2; i++ )
 {
 tem = a[i];
 a[i] = a[b -1];
 a[b -1] = tem;
 b--;
+}
+}
+else if (n % 2 == 1)
+{
+for (i = 0; i < (n -1)/2; i++ )
+{
+tem = a[i];
+a[i] = a[b -1];
+a[b -1] = tem;
+b--;
+}
 }
 }
--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -32,7 +32,7 @@ void print_array(int *a, int n)
  */
 int main(void)
 {
-    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 98, 1024, 1337};
 
     print_array(a, sizeof(a) / sizeof(int));
     reverse_array(a, sizeof(a) / sizeof(int));
