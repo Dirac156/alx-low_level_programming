beb8a20708875f2df99f785b342646a7cc18fd2b
Dirac Murairi <diracm8@gmail.com>
2020-10-02 22:14:56 +0200

    d

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,11 +12,14 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0; src[i] != '\0' && i < n; i++)
+for (i = 0; src[i] != '\0' && i < n; ++i)
 {
 dest[i] = src[i];
 }
-i++;
+while (i < n)
+{
 dest[i] = '\0';
+i++;
+}
 return (dest);
 }
