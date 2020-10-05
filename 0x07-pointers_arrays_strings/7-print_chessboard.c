bda791afa3e761d442412f55185967f96e9c6e83
Dirac Murairi <diracm8@gmail.com>
2020-10-05 22:17:25 +0200

    d

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,14 +11,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-char *b = *a;
 int i, c;
 for (i = 0; i < 8; i++)
 {
 for (c = 0; c < 8; c++)
 {
-_putchar(b[i] + '0');
+putchar(a[i][c]);
 }
-_putchar('\n');
+putchar('\n');
 }
 }
