74c6f14c98d5af7385621a6962f92624371bbd85
Dirac Murairi <diracm8@gmail.com>
2020-10-05 22:46:45 +0200

    d

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strspn - function
+ * @s : pointer to check if the value is inside
+ * @accept : the value to check
+ * Return: memory.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j;
+unsigned int count = 0;
+
+for (i = 0; s[i] != ',' ; i++)
+{
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (accept[j] == s[i])
+{
+count++;
+break;
+}
+}
+}
+return (count);
+}
--- 0x07-pointers_arrays_strings/main.c
+++ 0x07-pointers_arrays_strings/main.c
@@ -8,14 +8,11 @@
  */
 int main(void)
 {
-    char *s = "hello";
-    char *f;
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
 
-    f = _strchr(s, 'l');
-
-    if (f != NULL)
-    {
-        printf("%s\n", f);
-    }
+    n = _strspn(s, f);
+    printf("%u\n", n);
     return (0);
 }
