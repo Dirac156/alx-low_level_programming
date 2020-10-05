a97951c98f999664698d9be7bfcb75461123f5fa
Dirac Murairi <diracm8@gmail.com>
2020-10-05 14:54:20 +0200

    d

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
