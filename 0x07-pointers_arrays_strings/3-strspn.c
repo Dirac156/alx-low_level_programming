a3c3938202f4a4b35d7a1efb11eb058b4958d751
Dirac Murairi <diracm8@gmail.com>
2020-10-05 22:48:53 +0200

    d

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 int i, j;
 unsigned int count = 0;
 
-for (i = 0; s[i] != ',' ; i++)
+for (i = 0; s[i] != ',' && s[i] != '\n' && s[i] != ' ' ; i++)
 {
 for (j = 0; accept[j] != '\0'; j++)
 {
