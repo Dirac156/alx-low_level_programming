300c6a0b4693a6b5dff399f6213c674dd5822920
Dirac Murairi <diracm8@gmail.com>
2020-10-07 21:36:49 +0200

    d

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * _pow_recursion - check the code for Holberton School students.
+ *@n : parameter
+ *Return: lenfth of s
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+else if (y != 0)
+{
+return ( x * _pow_recursion(x, y-1));
+}
+return (0);
+}
