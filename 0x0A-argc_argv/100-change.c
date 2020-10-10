185459803f02f4bcc4fbec4d80fe678b592f6ddb
Dirac Murairi <diracm8@gmail.com>
2020-10-10 20:13:30 +0200

    D

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -28,67 +28,52 @@ printf("0\n");
 return (0);
 }
 
+/**
+  * checker - entry function
+  *@c : variable
+  *@b : variable
+  */
 
-void checker (int c, int b)
+void checker(int c, int b)
 {
 if (c >= 25)
 {
-b += 1;
 if (c - 25 == 0)
 {
-printf("%d\n", b);
+printf("%d\n", b + 1);
 }
-else
-{
-checker(c - 25, b);
-}
+checker(c - 25, b + 1);
 }
 else if (c >= 10)
 {
-b += 1;
 if (c - 10 == 0)
 {
-printf("%d\n", b);
+printf("%d\n", b + 1);
 }
-else
-{
-checker(c - 10, b);
-}
+checker(c - 10, b + 1);
 }
 else if (c >= 5)
 {
-b += 1;
 if (c - 5 == 0)
 {
-printf("%d\n", b);
+printf("%d\n", b + 1);
 }
-else
-{
-checker(c - 5, b);
-}
+checker(c - 5, b + 1);
 }
 else if (c >= 2)
 {
-b += 1;
 if (c - 2 == 0)
 {
-printf("%d\n", b);
+printf("%d\n", b + 1);
 }
-else
-{
-checker(c - 2, b);
-}
+checker(c - 2, b + 1);
 }
 else if (c >= 1)
 {
-b += 1;
 if (c - 1 == 0)
 {
-printf("%d\n", b);
+printf("%d\n", b + 1);
 }
-else
-{
-checker(c - 1, b);
-}
+checker(c - 1, b + 1);
 }
 }
