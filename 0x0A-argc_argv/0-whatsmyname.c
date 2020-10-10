75ffefae029c9f4268164487f5d75f3b7338901c
Dirac Murairi <diracm8@gmail.com>
2020-10-10 17:36:16 +0200

    d

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+  *main - entry
+  *@argv : the string from the terminal
+  *@argc : the number of arameters
+  *Return: 0
+  */
+
+int main(int argc, char const *argv[]) {
+  (void)argc;
+  printf("%s\n", argv[0]);
+  return 0;
+}
