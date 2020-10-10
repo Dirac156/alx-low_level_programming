9514c6d06679cf3054544210a6dd65742ecbe785
Dirac Murairi <diracm8@gmail.com>
2020-10-10 17:37:42 +0200

    d

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,8 +8,9 @@
   *Return: 0
   */
 
-int main(int argc, char const *argv[]) {
-  (void)argc;
-  printf("%s\n", argv[0]);
-  return 0;
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
 }
