bb4763cc68d8df7678cffa56a4aa898b88acc877
Dirac Murairi <diracm8@gmail.com>
2020-10-12 18:19:09 +0200

    d

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,3 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -7,7 +9,7 @@ int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
 char *_strcpy(char *dest, char *src);
-int _atoi(char *s); 
+int _atoi(char *s);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
