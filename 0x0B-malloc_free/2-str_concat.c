5547b7dbbb306d770b4568055356ab14d1b37560
Dirac Murairi <diracm8@gmail.com>
2020-10-13 20:35:07 +0200

    d

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,49 +1,48 @@
-#include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "holberton.h"
+
 /**
-  *str_concat - main function
-  *@s1 : parameter 1
-  *@s2 : parameter 2
-  *Return: character
-  */
+ * _strlen - main function
+ * @s: variable
+ * Return: return
+ */
+
+
+int _strlen(char *s)
+{
+int size = 0;
+for (; s[size] != '\0'; size++)
+;
+return (size);
+}
+
+/**
+ * *str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer
+ */
+
 char *str_concat(char *s1, char *s2)
 {
-int a = 0, b = 0;
-while (s1[a] != '\0')
-{
-a++;
-}
-while (s2[b] != '\0')
-{
-b++;
-}
-char *ss = malloc((a + b) * sizeof(char) + 1);
-int i = 0;
-if (ss == NULL)
-{
-return (NULL);
-}
+
+int size1 = _strlen(s1), size2 = _strlen(s2), i;
+char *m = malloc((size1 + size2) * sizeof(char) + 1);
+if (m == NULL)
+	return (NULL);
 if (s1 == NULL)
-{
-s1 = "";
-}
+	s1 = "";
 if (s2 == NULL)
+	s2 = "";
+
+for (i = 0; i <= size1 + size2; i++)
 {
-s2 = "";
+	if (i < size1)
+		m[i] = s1[i];
+	else
+		m[i] = s2[i - size1];
 }
-while (s1[i] != '\0')
-{
-ss[i] = s1[i];
-i++;
-}
-int j = 0;
-while (s2[j] != '\0')
-{
-ss[i] = s2[j];
-j++;
-i++;
-}
-ss[i] = '\0';
-return (ss);
+
+return (m);
 }
