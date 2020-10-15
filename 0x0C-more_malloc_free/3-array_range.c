01be20598ca2027f3665bc35cd2862a5da010ccb
Dirac Murairi <diracm8@gmail.com>
2020-10-15 16:05:24 +0200

    d

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+  *array_range- check if malloc library is assigning memories
+  *@min: variable
+  *@max: parameter 2
+  *Return: integer
+  */
+
+int *array_range(int min, int max)
+{
+int i = 0, t;
+int *ptr;
+if (min > max)
+{
+return (NULL);
+}
+ptr = malloc(sizeof(int) * (max - min) + 1);
+if (ptr == NULL)
+{
+return (NULL);
+}
+t = min;
+while (i < max - min)
+{
+ptr[i] = t;
+t++;
+i++;
+}
+return (ptr);
+}
--- 0x0C-more_malloc_free/main.c
+++ 0x0C-more_malloc_free/main.c
@@ -10,7 +10,7 @@
  *
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+void simple_print_buffer(int *buffer, unsigned int size)
 {
     unsigned int i;
 
@@ -38,13 +38,10 @@ void simple_print_buffer(char *buffer, unsigned int size)
  */
 int main(void)
 {
-    char *a;
+    int *a;
 
-    a = _calloc(98, sizeof(char));
-    strcpy(a, "Holberton");
-    strcpy(a + 9, " School! :)\n");
-    a[97] = '!';
-    simple_print_buffer(a, 98);
+    a = array_range(0, 10);
+    simple_print_buffer(a, 11);
     free(a);
     return (0);
 }
