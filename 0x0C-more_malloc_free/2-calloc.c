2f34f54a1e60d16e5c5c1b42cc1c70a540376a53
Dirac Murairi <diracm8@gmail.com>
2020-10-15 15:45:06 +0200

    d

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,18 +6,19 @@
   *Return: integer
   */
 
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int count = 0;
 if (size == 0 || nmemb == 0)
+{
 return (NULL);
-
+}
 char *ptr = malloc(nmemb * size);
 if (ptr == NULL)
+{
 return (NULL);
-
-while (count < nmemb * size)
+}
+while (count <= nmemb * size)
 {
 ptr[count] = 0;
 count++;
