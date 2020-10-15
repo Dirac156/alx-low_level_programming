6b208d1ce39f1dae14a9e1548ffcd502a678e0eb
Dirac Murairi <diracm8@gmail.com>
2020-10-15 15:17:25 +0200

    D

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 /**
   *string_nconcat - check if malloc library is assigning memories
   *@s1: variable
-  *@S2 : parameter 2
+  *@s2: parameter 2
   *@n : n charachter
   *Return: integer
   */
@@ -12,19 +12,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 char *concat_str;
 unsigned int concat_index = 0, lens2 = 0, lens1 = 0, idx = 0;
 if (s1 == NULL)
-	s1 = "";
+s1 = "";
 if (s2 == NULL)
-	s2 = "";
-for (; s2[lens1] != '\0';lens1++)
-  ;
-for (; s2[lens2] != '\0';lens2++)
-  ;
+s2 = "";
+for (; s2[lens1] != '\0'; lens1++)
+;
+for (; s2[lens2] != '\0'; lens2++)
+;
 if (n > lens2)
-  concat_str = malloc(sizeof(char *) * (lens1 + lens2 + 1));
+concat_str = malloc(sizeof(char *) * (lens1 + lens2 + 1));
 else
-  concat_str = malloc(sizeof(char) * (lens1 + n + 1));
+concat_str = malloc(sizeof(char) * (lens1 + n + 1));
 if (concat_str == NULL)
-  return (NULL);
+return (NULL);
 
 while (s1[concat_index] != '\0')
 {
@@ -43,8 +43,7 @@ else
 {
 while (idx < n && s2[idx] != '\0')
 {
-concat_str[concat_index] = s2[idx];
-idx++;
+concat_str[concat_index] = s2[idx++];
 concat_index++;
 }
 }
