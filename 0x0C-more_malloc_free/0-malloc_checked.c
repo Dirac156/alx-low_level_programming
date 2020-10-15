4a485f2c253301e779e98b81fe322c16a84660e6
Dirac Murairi <diracm8@gmail.com>
2020-10-15 12:06:13 +0200

    d

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+  *malloc_checked - check if malloc library is assigning memories
+  *@b: variable
+  */
+
+void *malloc_checked(unsigned int b)
+{
+int a = 98;
+int *status = &a;
+void *ptr =  malloc(b);
+if (ptr != NULL)
+{
+return (ptr);
+}
+return (status);
+}
---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,5 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+#endif
