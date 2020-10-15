24dd497e76566704489ae93d2ac793e03c9b47cc
Dirac Murairi <diracm8@gmail.com>
2020-10-15 15:30:43 +0200

    d

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,8 +11,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 if (size == 0 || nmemb == 0)
 return (NULL);
-void *ptr = malloc(nmemb * size);
-if (!ptr)
+void *ptr = malloc(nmemb * size + 1);
+if (ptr == NULL)
 return (NULL);
 return (ptr);
 }
