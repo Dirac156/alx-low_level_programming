4d577d0965b13e1f1ccd6c1db5abf50e78d26fbb
Dirac Murairi <diracm8@gmail.com>
2020-10-20 18:04:52 +0200

    d

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+  *init_dog - a function that initialize a Structure
+  *@d : our Structure
+  *@name : the name we are going to attribute
+  *@age : the age we are going to attribute
+  *@owner : the owner name we are going to attribute
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d.name = name;
+d.age = age;
+d.owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,5 +14,5 @@ char *name;
 float age;
 char *owner;
 } dog_t;
-
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
