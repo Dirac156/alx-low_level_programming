519e88935b827186ad09141fa0cdb0914b91f95d
Dirac Murairi <diracm8@gmail.com>
2020-10-20 18:24:24 +0200

    solve issue with null structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,22 +7,25 @@
 
 void print_dog(struct dog *d)
 {
+if (d != NULL)
+{
 if (d->name != NULL)
 {
-printf("%s\n", d->name);
+printf("Name : %s\n", d->name);
 }
 else
 printf("nil\n");
 if (d->age != 0.0)
 {
-printf("%f\n", d->age);
+printf("Age : %f\n", d->age);
 }
 else
 printf("nil\n");
 if (d->owner != NULL)
 {
-printf("%s\n", d->owner);
+printf("Owner : %s\n", d->owner);
 }
 else
 printf("nil\n");
+}
 }
