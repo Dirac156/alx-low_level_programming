5f917320f9deaae0f56e84731c28bbc58d71d3d8
Dirac Murairi <diracm8@gmail.com>
2020-10-20 18:26:00 +0200

    fix the output of all null variables

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,18 +14,18 @@ if (d->name != NULL)
 printf("Name : %s\n", d->name);
 }
 else
-printf("nil\n");
+printf("Name : nil\n");
 if (d->age != 0.0)
 {
 printf("Age : %f\n", d->age);
 }
 else
-printf("nil\n");
+printf("Age : nil\n");
 if (d->owner != NULL)
 {
 printf("Owner : %s\n", d->owner);
 }
 else
-printf("nil\n");
+printf("Owner : nil\n");
 }
 }
