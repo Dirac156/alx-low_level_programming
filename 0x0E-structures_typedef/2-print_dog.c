2dc26378bdf418dd2e927e36e55df86e975c8f34
Dirac Murairi <diracm8@gmail.com>
2020-10-20 18:48:06 +0200

    d

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,18 @@ void print_dog(struct dog *d)
 {
 if (d != NULL)
 {
-printf("Name: %s\nAge: %f\nOwner: %s\n",
-(*d).name != NULL ? d->name : "nil",
-(*d).age,
-(*d).owner != NULL ? d->owner : "nill");
+if (d->name != NULL)
+{
+printf("Name: %s\n", d->name);
+}
+else
+printf("Name: nil\n");
+printf("Age: %f\n", (*d).age);
+if (d->owner != NULL)
+{
+printf("Owner: %s\n", d->owner);
+}
+else
+printf("Owner: nil\n");
 }
 }
