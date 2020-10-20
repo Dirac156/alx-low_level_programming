3437e96e934b277a2bc995f3883148aebc9186d4
Dirac Murairi <diracm8@gmail.com>
2020-10-20 17:55:37 +0200

    d

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - my dog data type
+ * @name: the name of the dog which is an array
+ * @age: the age of the dog which is an integer
+ * @owner: The owner of the dog which is an char array
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+#endif
