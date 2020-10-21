bc57dacf8be346f505b24a34cc43566af90076c1
Dirac Murairi <diracm8@gmail.com>
2020-10-21 22:13:17 +0200

    initialized files and submit assignment 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+  *print_name - use the function passed to it to print the print_name
+  *@name : The name we want to print
+  *@f : the function that will print the print
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,5 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+#endif
