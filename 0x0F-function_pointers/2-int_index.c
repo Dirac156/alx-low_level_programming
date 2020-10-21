3bdd3a7dd69d618a1146d9ebde48d45715c6b311
Dirac Murairi <diracm8@gmail.com>
2020-10-21 23:23:22 +0200

    fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,9 +9,11 @@
   */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-if (size <= 0 || array == NULL || cmp == NULL)
-return (-1);
 int i;
+if (size <= 0 || array == NULL || cmp == NULL)
+{
+return (-1);
+}
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]) != 0)
