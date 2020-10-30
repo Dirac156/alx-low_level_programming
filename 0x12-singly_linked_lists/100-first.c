abf6dc47a3f4f469ed252f13901bdf7f64110917
Dirac Murairi <diracm8@gmail.com>
2020-10-30 22:27:03 +0200

    submit advanced task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+  *myStartupFun - executed before main
+  */
+void myStartupFun(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -14,9 +14,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 int _putchar(char c);
 size_t print_list(const list_t *h);
@@ -25,4 +25,5 @@ list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *s);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void myStartupFun(void) __attribute__ ((constructor));
 #endif
