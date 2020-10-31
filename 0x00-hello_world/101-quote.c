6c1ea367a225cee2466c1ff6d6a7ac6ab2431647
Dirac Murairi <diracm8@gmail.com>
2020-10-31 06:32:57 +0200

    tried to fix the error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(2, ch, sizeof(ch));
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch) - 1);
 return (1);
 }
