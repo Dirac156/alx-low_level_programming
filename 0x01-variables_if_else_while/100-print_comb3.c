985eeed34f6bcea5a1d45e8098e8d92265c7e248
Dirac Murairi <diracm8@gmail.com>
2020-10-31 08:10:31 +0200

    Complete the advanced task 0

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main -  prints all possible different combinations of two digits
+ *depending on some conditions
+ * Return:0
+ */
+
+int main(void)
+{
+int x, y;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y < 10; y++)
+{
+if (y > x)
+{
+putchar(x + '0');
+putchar(y + '0');
+if (x != 8 || y != 9)
+{
+putchar (',');
+putchar (' ');
+}
+}
+}
+}
+putchar('\n');
+return 0;
+}
