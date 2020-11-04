3760daa47bdbe2c6e3a353664d4725a4e6e8ef42
Dirac Murairi <diracm8@gmail.com>
2020-11-05 01:03:05 +0200

    Link the new node with the null pointer after the definition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,13 +9,12 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int i = 0;
-listint_t *tm;
-listint_t *new_node;
-listint_t *last_node;
+listint_t *tm, *new_node, *last_node;
 new_node = (listint_t *)malloc(sizeof(listint_t));
 if (!new_node)
 return (NULL);
 new_node->n = n;
+new_node->next = NULL;
 if (!*head)
 return (NULL);
 else
