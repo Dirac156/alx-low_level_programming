97b04cd683abb3b98044365c14b1f588ae637ab4
Dirac Murairi <diracm8@gmail.com>
2020-11-04 09:57:03 +0200

    initial submission of task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  *print_listint - The function we will use to print all integer.
+  *@h: The linked list where we will use to print its elements
+  *Return: The number of elements in the lined list
+  */
+
+size_t print_listint (const listint_t *h)
+{
+    size_t i = 0;
+    const listint_t *ptr = h;
+    while (ptr != NULL)
+    {
+      printf("%d\n", ptr->n);
+      ptr = ptr->next;
+      i++;
+    }
+    return (i);
+}
