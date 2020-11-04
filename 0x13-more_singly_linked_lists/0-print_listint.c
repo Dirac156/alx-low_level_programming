526943fe62d463765dc3d186e48db8a809974dfa
Dirac Murairi <diracm8@gmail.com>
2020-11-04 11:00:41 +0200

    Fix indentation error with betty style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,14 @@
 
 size_t print_listint (const listint_t *h)
 {
-    size_t i = 0;
-    const listint_t *ptr = h;
-    while (ptr != NULL)
-    {
-      printf("%d\n", ptr->n);
-      ptr = ptr->next;
-      i++;
-    }
-    return (i);
+	size_t i = 0;
+	const listint_t *ptr = h;
+
+	while (ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
 }
