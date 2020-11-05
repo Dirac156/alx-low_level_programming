d6fc91f225084d3ff61cdae139475e09473d5e75
Dirac Murairi <diracm8@gmail.com>
2020-11-05 16:52:29 +0200

    initial submition task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+#include <math.h>
+/**
+  *binary_to_uint - change a binary to decimal number
+  *@b: the binary to change
+  *Return: the decimal number
+  */
+unsigned int binary_to_uint(const char *b)
+{
+int i = 0, time, mult = 1;
+unsigned int number = atoi(b);
+unsigned int dec = 0, rem;
+if (!b)
+return (0);
+for (; b[i] != '\0'; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+}
+i = 0;
+while (number != 0)
+{
+time = 0;
+mult = 1;
+rem = number % 10;
+number = number / 10;
+while (time < i)
+{
+time++;
+mult = mult * 2;
+}
+if (rem != 0)
+{
+dec += rem * mult;
+}
+i++;
+}
+return (dec);
+}
---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+unsigned int binary_to_uint(const char *b);
+
+#endif
