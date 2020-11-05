8240e50c65c56414694be573936e8da544a0fcac
Dirac Murairi <diracm8@gmail.com>
2020-11-05 20:36:16 +0200

    subnit advanced task

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+  *print_binary - a function that display binaries
+  *@n : the integer we want to change to binary
+  */
+void print_binary(unsigned long int n)
+{
+unsigned int c;
+unsigned int display_mask = 1 << 31;
+for (c = 1; c < 32; ++c)
+{
+_putchar(n & display_mask? '1': '0');
+n <<= 1;
+}
+_putchar('\n');
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -4,5 +4,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
+int _putchar(char c);
 #endif
