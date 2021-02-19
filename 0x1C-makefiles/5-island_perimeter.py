ceae07daf5c23c17976000bfb824946a927111cc
= <=>
2021-02-19 22:29:04 +0200

    kwame is dancing

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,32 +6,23 @@ island perimeter
 
 def island_perimeter(grid):
     """return the perimeter of the island"""
-    perimeter = False
-    first = False
-    isIland = False
-    isSameArray = False
+    perimeter = 0
+    first = 0
+    isIland = 0
     if grid is None:
-        return
+        return 0
     elif grid == [[]]:
         return 0
     else:
-        i = 0
         for lst in grid:
-            j = 0
             for numb in lst:
                 if numb == 1:
-                    j += 1
-                    isIland = True
-                    if first is False:
-                        first = True
+                    isIland = 1
+                    if first == 0:
+                        first = 1
                         perimeter += 3
                     else:
                         perimeter += 2
-            if j > 1 and i == 0:
-                isSameArray = True
-            else:
-                isSameArray = False
-            i += 1
-        if isIland is True and isSameArray is False:
+        if isIland == 1:
             perimeter += 1
     return perimeter
