e28b71ead0bb209925514e67c9209f970cee5db6
= <=>
2021-02-24 11:42:21 +0200

    styles

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-int *ptr, i;
+int *ptr;
 
 if (nmemb == 0 || size == 0)
 return (NULL);
@@ -23,4 +23,23 @@ return (NULL);
 _memset((void *)ptr, 0, size * nmemb);
 
 return (ptr);
+}
+
+
+/**
+ * _memset - fills memory with a constant byte
+ * @new_ptr: the pointer
+ * @c: the char
+ * @size: the amount of memory being filled
+ * Return: the new memory
+ */
+char *_memset(char *new_ptr, char c, unsigned int size)
+{
+char *ptr = new_ptr;
+
+while (size--)
+{
+*ptr++ = c;
+}
+return (new_ptr);
 }
--- 0x0C-more_malloc_free/_memset.c
+++ 0x0C-more_malloc_free/_memset.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
  * _memset - fills memory with a constant byte
- * @s: the area being looked at
- * @b: the byte filling memory
- * @n: the amount of memory being filled
+ * @new_ptr: the pointer
+ * @c: the char
+ * @size: the amount of memory being filled
  * Return: the new memory
  */
 char *_memset(char *new_ptr, char c, unsigned int size)
